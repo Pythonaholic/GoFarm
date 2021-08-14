24fc3f2c86b3a2ac722aca860a34ccbd3f3d33f8
OBADA ALHAWJREH <obada7jaber7@gmail.com>
2021-08-14 19:15:56 +0300

    better try

--- component/footer.js
+++ component/footer.js
@@ -13,51 +13,53 @@ function Footer() {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossOrigin="anonymous" />
-          <script src="https://kit.fontawesome.com/752510c1eb.js" crossorigin="anonymous"></script>
-          <title>GoFarm</title>
+          <title>Product Page | Gelso Designs</title>
           <section className="footer-section">
             <div className="footer">
               <div className="social">
-                
-                <a href="/component/home.js"><i className="fas fa-home logo" /><i class="fas fa-tree "></i>GoFarm</a>
-                
-                
+                <a href="#header"><i className="fas fa-home logo" /><i class="fas fa-tree "></i>GoFarm<br></br></a>
                 <ul>
                   <li>
-                    <a href="https://twitter.com" target="_blank"><i className="fab fa-twitter-square" /></a>
+                    <a href="https://twitter.com/ancientnutr" target="_blank"><i className="fab fa-twitter-square" /></a>
                   </li>
                   <li>
-                    <a href="https://www.instagram.com" target="_blank"><i className="fab fa-instagram" /></a>
+                    <a href="https://www.instagram.com/explore/tags/modernsuperfood/" target="_blank"><i className="fab fa-instagram" /></a>
                   </li>
                   <li>
-                    <a href="https://www.facebook.com" target="_blank"><i className="fab fa-facebook-square" /></a>
+                    <a href="https://www.facebook.com/ancientnutrition/" target="_blank"><i className="fab fa-facebook-square" /></a>
+                  </li>
+                </ul>
+                <p><br></br><br></br>Find Your Best Farm.</p>
+              </div>
+              <div className="footer-links">
+                <h4>Links</h4>
+                <ul>
+                  <li>
+                    <a href="#benefits">Benefits</a>
+                  </li>
+                  <li>
+                    <a href="#products">Products</a>
+                  </li>
+                  <li>
+                    <a href="#demo-video">Video</a>
+                  </li>
+                  <li>
+                    <a href="#shopping-cart">Cart</a>
                   </li>
                 </ul>
               </div>
-              <div className="footer-links">
-
-              
-                
-              
-                <p><br></br>Find The Best Farm .</p>
-                
- 
-                
-               
-
-              </div>
               <div className="email-signup">
-                <h4>Subscribe</h4>
+                <h4>Sign up</h4>
                 <form id="form" action="https://www.freecodecamp.com/email-submit">
-                  <label>Sign up and stay up-to-date with new <br />locations, tips, news and Offers.</label><br />
+                  <label>Sign up and stay up-to-date with new <br />products, tips, news and recepies.</label><br />
                   <div className="section-wrapper">
-                    <input id="email" type="email" name="email" placeholder="Enter email..." required />
-                    <input id="submit" className="btn-ghost" type="submit" defaultValue="Sign up" />
+                    
+                    <button id="submit" className="btn-ghost" type="submit" defaultValue="Sign up">Sign up</button>
                   </div>
                 </form> 
               </div>
             </div>
-            <p className="footer-credit">Copyright © 2021 Designed by Gelso Designs and modified by GoFarm Team. All rights reserved. Website content taken from <a href="http://ancientnutrition.com/" target="_blank">Ancient Nutrition</a>.</p>
+            <p className="footer-credit">Copyright © 2021 Designed by GoFarm Team. All rights reserved. Find Your Best Farm.</p>
           </section>
         </div>
       );
