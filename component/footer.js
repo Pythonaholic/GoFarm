04c0d5051e24d05cfa8a8d6108f61002815c70fe
HamzaQahoush <hamza.qah@gmail.com>
2021-08-16 20:20:47 +0300

    last touches on Homepage

--- component/footer.js
+++ component/footer.js
@@ -1,81 +1,114 @@
-import React from 'react'
+import React from 'react';
 import { SettingsContext } from '../context/auth';
 import { FarmsContext } from '../context/farms';
 import { useContext } from 'react';
 
-
 function Footer() {
-    const context = useContext(SettingsContext);
-    const farmsListContext = useContext(FarmsContext)
+  const context = useContext(SettingsContext);
+  const farmsListContext = useContext(FarmsContext);
 
-    return (
-        <div className = 'footer'>
-          <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossOrigin="anonymous" />
-          <section className="footer-section">
-            <div className="footer">
-              <div className="social">
-                <a href="/"><img src="https://i.ibb.co/bsP31TV/black-s.png" className='photo'  alt="" /><br></br></a>
-                
-                <ul>
-                  <li>
-                    <a href="https://twitter.com" target="_blank"><i className="fab fa-twitter-square" /></a>
-                  </li>
-                  <li>
-                    <a href="https://www.instagram.com" target="_blank"><i className="fab fa-instagram" /></a>
-                  </li>
-                  <li>
-                    <a href="https://www.facebook.com" target="_blank"><i className="fab fa-facebook-square" /></a>
-                    
-                  </li>
-                  <li>
-                    <a href="https://github.com/" target="_blank"><i className="fab fa-github" /></a>
-        
-                  </li>
-                  <li>
-                    <a href="https://web.facebook.com" target="_blank"><i className="fab fa-facebook-messenger" /></a>
-        
-                  </li>
-                  <li>
-                    <a href="https://www.google.jo/" target="_blank"><i className="fab fa-google-drive" /></a>
-        
-                  </li>
-                </ul>
-                
+  return (
+    <div className="footer">
+      <link
+        href="https://fonts.googleapis.com/css?family=Lato:300,400"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+        integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+        crossOrigin="anonymous"
+      />
+      <section className="footer-section">
+        <div className="footer">
+          <div className="social">
+            <a href="/">
+              <img
+                className="photo"
+                src="https://i.ibb.co/HTW2LF2/logo2.png"
+                alt=""
+              />
+              <br></br>
+            </a>
+
+            <ul>
+              <li>
+                <a href="https://twitter.com" target="_blank">
+                  <i className="fab fa-twitter-square" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com" target="_blank">
+                  <i className="fab fa-instagram" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com" target="_blank">
+                  <i className="fab fa-facebook-square" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/" target="_blank">
+                  <i className="fab fa-github" />
+                </a>
+              </li>
+              <li>
+                <a href="https://web.facebook.com" target="_blank">
+                  <i className="fab fa-facebook-messenger" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.google.jo/" target="_blank">
+                  <i className="fab fa-google-drive" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-links">
+            <h4>Links</h4>
+            <ul>
+              <li>
+                <a href="#benefits">Benefits</a>
+              </li>
+              <li>
+                <a href="#products">Products</a>
+              </li>
+              <li>
+                <a href="#demo-video">Video</a>
+              </li>
+              <li>
+                <a href="#shopping-cart">Cart</a>
+              </li>
+            </ul>
+          </div>
+          <div className="email-signup">
+            <h4>Sign up</h4>
+            <form id="form" action="">
+              <label>
+                Sign up and stay up-to-date with new <br />
+                products, tips, news and recepies.
+              </label>
+              <br />
+              <div className="section-wrapper">
+                <button
+                  id="submit"
+                  className="btn-ghost"
+                  type="submit"
+                  defaultValue="Sign up"
+                >
+                  Sign up
+                </button>
               </div>
-              <div className="footer-links">
-                <h4>Links</h4>
-                <ul>
-                  <li>
-                    <a href="#benefits">Benefits</a>
-                  </li>
-                  <li>
-                    <a href="#products">Products</a>
-                  </li>
-                  <li>
-                    <a href="#demo-video">Video</a>
-                  </li>
-                  <li>
-                    <a href="#shopping-cart">Cart</a>
-                  </li>
-                </ul>
-              </div>
-              <div className="email-signup">
-                <h4>Sign up</h4>
-                <form id="form" action="https://www.freecodecamp.com/email-submit">
-                  <label>Sign up and stay up-to-date with new <br />products, tips, news and recepies.</label><br />
-                  <div className="section-wrapper">
-                    
-                    <button id="submit" className="btn-ghost" type="submit" defaultValue="Sign up">Sign up</button>
-                  </div>
-                </form> 
-              </div>
-            </div>
-            <p className="footer-credit">Copyright © 2021 Designed by GoFarm Team. All rights reserved. Find Your Best Farm.</p>
-          </section>
-          
+            </form>
+          </div>
         </div>
-      );
+        <p className="footer-credit">
+          Copyright © 2021 Designed by GoFarm Team. All rights reserved. Find
+          Your Best Farm.
+        </p>
+      </section>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,31 @@
-import 'tailwindcss/tailwind.css'
-import SettingsProvider from '../context/auth'
-import FarmsProvider from '../context/farms'
-import '../styles/footer.scss'
-import '../styles/searchform.scss'
-import '../styles/searchresult.scss'
-import '../styles/form.scss'
-import '../styles/extraformcss.scss'
-import '../styles/sign.scss'
-import '../styles/detailsinfo.scss'
-import '../styles/rivewcom.scss'
-import '../styles/myfarms.scss'
+import 'tailwindcss/tailwind.css';
+import SettingsProvider from '../context/auth';
+import FarmsProvider from '../context/farms';
+import '../styles/footer.scss';
+import '../styles/searchform.scss';
+import '../styles/searchresult.scss';
+import '../styles/form.scss';
+import '../styles/extraformcss.scss';
+import '../styles/sign.scss';
+import '../styles/detailsinfo.scss';
+import '../styles/rivewcom.scss';
+import '../styles/myfarms.scss';
 import '../styles/header_main.scss';
 import '../styles/main_comp.scss';
 import '../styles/slider_main.scss';
+import '../styles/video_main.scss';
+
 import toast, { Toaster } from 'react-hot-toast';
 
-
 function MyApp({ Component, pageProps }) {
-
   return (
     <SettingsProvider>
       <FarmsProvider>
         <Component {...pageProps} />
-        <Toaster
-          position="bottom-right"
-          reverseOrder={false}
-        />
+        <Toaster position="bottom-right" reverseOrder={false} />
       </FarmsProvider>
     </SettingsProvider>
-  )
-
+  );
 }
 
-export default MyApp
+export default MyApp;
